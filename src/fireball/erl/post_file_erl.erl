e0a414b794ef09f3f44a55ee165b665c7af0b999
madelinehebert <madelinehebert@proton.me>
2024-11-27 00:43:04 -0500

    Wrote erlang test code to start and stop inets and ssl.

---
+++ src/fireball/erl/post_file_erl.erl
@@ -0,0 +1,15 @@
+-module(post_file_erl).
+-export([post_file_erl/2]).
+
+% A function to upload a file to Google Firebase Storage.
+% Takes a URL and a file location on disk.
+post_file_erl(URL, File) ->
+    % Start inets and ssl
+    inets:start(),
+    ssl:start(),
+
+    % Send the file!
+
+    % Stop inets and ssl
+    ssl:stop(),
+    inets:stop().
